8f49efc2a5f8092ea9b57a4380b52a8b50c2b55b
Dagna Schmidt <dagdagi889@gmail.com>
2023-06-21 13:55:09 +0200

    part8 8.8

--- frontend/src/components/Authors.js
+++ frontend/src/components/Authors.js
@@ -1,9 +1,20 @@
+import { useQuery } from "@apollo/client";
+
+import { ALL_PERSONS } from "../queries";
+import { useState } from "react";
+
 const Authors = (props) => {
+  const [authors, setAuthors] = useState([]);
+  const result = useQuery(ALL_PERSONS);
+  console.log(result);
+
+    useState(() => {
+      setAuthors(result.data.allAuthors);
+    }, []);
+  
     if (!props.show) {
       return null
     }
-    const authors = []
-  
     return (
       <div>
         <h2>authors</h2>
@@ -14,7 +25,7 @@ const Authors = (props) => {
               <th>born</th>
               <th>books</th>
             </tr>
-            {authors.map((a) => (
+            {authors.length !== 0 && authors.map((a) => (
               <tr key={a.name}>
                 <td>{a.name}</td>
                 <td>{a.born}</td>
